f3cde27a7d0deb51e81835a3db99521d1a48280f
Alessandro Magionami <alessandro.magionami@gmail.com>
2025-08-21 11:45:06 +0200

    chore: initial setup for plugin

---
+++ index.ts
@@ -0,0 +1,95 @@
+import type {
+  FastifyReply,
+  FastifyRequest,
+  RouteGenericInterface,
+  onRequestHookHandler,
+} from "fastify";
+import fastifyPlugin from "fastify-plugin";
+import PocketBase, { RecordService } from "pocketbase";
+
+interface Options {
+  pocketbaseUrl: string
+  superuserEmail: string
+  superuserPassword: string
+  authCollectionName?: string
+}
+
+export const authPlugin = fastifyPlugin<Options>(
+  async (fastify, opts) => {
+    const authCollectionName = opts.authCollectionName ?? 'users'
+    const pb = new PocketBase(opts.pocketbaseUrl);
+    const authCheck = new PocketBase(opts.pocketbaseUrl);
+
+    await pb
+      .collection("_superusers")
+      .authWithPassword(
+        opts.superuserEmail,
+        opts.superuserPassword,
+        {
+          // This will trigger auto refresh or auto reauthentication in case
+          // the token has expired or is going to expire in the next 30 minutes.
+          autoRefreshThreshold: 30 * 60,
+        },
+      );
+    pb.autoCancellation(false);
+
+    fastify.decorate("authCollection", pb.collection(authCollectionName));
+    fastify.decorate(
+      "authenticate",
+      async ({ email, password }: { email: string; password: string }) => {
+        const { record } = await authCheck
+          .collection(authCollectionName)
+          .authWithPassword(email, password);
+        authCheck.authStore.clear();
+        return record?.id;
+      },
+    );
+
+    await fastify.register(jwt, {
+      secret: fastify.config.JWT_SECRET,
+    });
+
+    fastify.decorateRequest("beta", false);
+
+    fastify.addHook<RouteGenericInterface, { checkAuthentication?: boolean }>(
+      "onRoute",
+      async (routeOptions) => {
+        if (!routeOptions.config?.checkAuthentication) {
+          return;
+        }
+
+        async function onRequest(req: FastifyRequest, reply: FastifyReply) {
+          const userId = await req.getUserId();
+          const user = await fastify.authCollection.getOne(userId);
+          if (!user) {
+            reply.statusCode = 401;
+            throw new Error("Unauthenticated");
+          }
+        }
+
+        routeOptions.onRequest = [
+          ...((routeOptions.onRequest as onRequestHookHandler[] | undefined) ||
+            []),
+          onRequest,
+        ];
+      },
+    );
+  },
+  {
+    decorators: { request: ["getUserId"] },
+    name: "pocketbase-auth",
+  },
+);
+
+declare module "fastify" {
+  interface FastifyInstance {
+    authCollection: RecordService<{ id: string; email: string; password: string }>;
+    authenticate: (
+      user: { id: string; email: string; password: string },
+    ) => Promise<string | undefined>;
+  }
+  interface FastifyRequest {
+    getUserId: () => string | Promise<string>
+  }
+}
+
